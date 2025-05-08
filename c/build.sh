bf8dee99487507fee85dfc7235cdf6be41e74844
Pedro <pedro.loureiro@diffusiondata.com>
2025-05-08 18:57:05 +0000

    Updated Diffusion Examples to 6.12.0-73138.9bc5b421-SNAPSHOT

--- c/build.sh
+++ c/build.sh
@@ -27,28 +27,51 @@ function build_for_unix() {
     local operating_system=$2
     local is_macos=$3
     local cmake_dir="$TARGET_DIR/cmake-$operating_system-$architecture"
+    echo "About to run cmake for $operating_system / $architecture"
+    echo "Build directory: $cmake_dir"
 
-    if $is_macos ; then
-        if [[ "$architecture" == "x86_64" ]]; then
-            /usr/local/bin/brew shellenv
+    # Define reusable build steps
+    build_commands() {
+        set -e
+        echo "Running cmake for $operating_system / $architecture"
+        echo "Build directory: $cmake_dir"
+        echo "---"
+        set -x
 
-        elif [[ "$architecture" == "arm64" ]]; then
-            /opt/homebrew/bin/brew shellenv
+        if $is_macos ; then
+            if [[ "$architecture" == "x86_64" ]]; then
+                /usr/local/bin/brew shellenv
+
+            elif [[ "$architecture" == "arm64" ]]; then
+                /opt/homebrew/bin/brew shellenv
+            fi
         fi
+
+        cmake --fresh \
+            -S"$SOURCE_DIR" \
+            -B"$cmake_dir" \
+            -DTARGET="$TARGET_DIR" \
+            -DOPERATING_SYSTEM="$operating_system" \
+            -DARCHITECTURE="$architecture" \
+            -DARCHFLAGS="-arch ${architecture}" \
+            -DCMAKE_OSX_ARCHITECTURES="${architecture}"
+
+        cmake --build "$cmake_dir"
+        set +x
+    }
+
+    if $is_macos; then
+        export SOURCE_DIR TARGET_DIR cmake_dir operating_system architecture is_macos
+
+        # Pass function definition and invoke it inside the new shell
+        set -x
+        arch "-$architecture" /bin/zsh --login -c "$(declare -f build_commands); source ~/.zshrc && source ~/.profile && source ~/.zprofile && set -a; SOURCE_DIR=${SOURCE_DIR}; is_macos=${is_macos}; TARGET_DIR=${TARGET_DIR}; cmake_dir=${cmake_dir}; operating_system=${operating_system}; architecture=${architecture}; build_commands" || exit 1
+        set +x
+    else
+        build_commands
     fi
 
-    # shellcheck disable=SC2086
-    arch -$architecture cmake --fresh           \
-        -S"$SOURCE_DIR"                         \
-        -B"$cmake_dir"                          \
-        -DTARGET="$TARGET_DIR"                  \
-        -DOPERATING_SYSTEM="$operating_system"  \
-        -DARCHITECTURE="$architecture"
-
-    # shellcheck disable=SC2086
-    arch -$architecture cmake --build "$cmake_dir"
 }
-
 
 
 case "$OSTYPE" in
