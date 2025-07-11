b5ab6d5c6dc9b70da8c44fb06f73de1a3ad805f2
Pedro <pedro.loureiro@diffusiondata.com>
2025-07-11 11:50:28 +0000

    Updated Diffusion Examples to 6.12.0-82055.b30d0232-SNAPSHOT

--- android/pubsub/app/src/main/java/com/pushtechnology/diffusion/examples/PubSubExample.java
+++ android/pubsub/app/src/main/java/com/pushtechnology/diffusion/examples/PubSubExample.java
@@ -85,7 +85,8 @@ public class PubSubExample extends Activity {
             if (ex != null) {
                 Log.e("diffusion", "Failed to connect to Diffusion server, is it running? Will retry.", ex);
                 executor.schedule(this::startSession, 10, TimeUnit.SECONDS);
-            } else {
+            }
+            else {
                 example(session);
             }
         });
@@ -141,7 +142,7 @@ public class PubSubExample extends Activity {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        TextView valueView = (TextView)findViewById(R.id.value);
+                        final TextView valueView = (TextView)findViewById(R.id.value);
                         valueView.setText(String.format(Locale.getDefault(),
                             "Subscribed to 'counter' topic: %d", newValue));
                     }
